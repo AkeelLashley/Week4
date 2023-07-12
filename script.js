e14cec041008b11e59d5bde3d625a5c0d8bc6667
Akeel Lashley <akeel@futuretek.ca>
2023-07-12 10:30:23 -0400

    localStorage

--- script.js
+++ script.js
@@ -130,16 +130,167 @@ function randomNumberFunction() {
 
 }
 
-randomNumberFunction()
+
+function formValidation() {
+let userName = document.getElementById("name")
+let age = document.getElementById("age")
+let submitButton = document.getElementById("submit")
+let errorMsg = document.getElementById("verify")
+let welcomeMsg = document.getElementById("welcome")
 
 
-// && || operator
+
+function handleSubmit() {
+    errorMsg.textContent = ""
+    welcomeMsg.textContent = ""
+     userInputName = userName.value.trim()
+    userInputAge = parseInt(age.value)
+
+    if(userInputName === "") {
+        errorMsg.textContent = "Please Input values"
+        return
+    } 
+
+    // Returns the data type of a variable
+    console.log(typeof userInputAge)
+
+    if(userInputName === "Akeel" && userInputAge === 24 ) {
+        welcomeMsg.textContent = "Welcome Akeel"
+    } else {
+        errorMsg.textContent = "Goodbye Stranger"
+    }
+   
+}
+
+
+submitButton.addEventListener("click", function() {
+       handleSubmit()
+})
+
+}
+
+
+
 
 // innerHTML
+//*********************UPDATING INNER HTML*****************
+// 2. Grab the unordered list and store it in a const variable called ulEl
+const ulEl = document.getElementById("ul-el");
+let colour = document.getElementById("colour");
+let addBtn = document.getElementById("add")
+
+let shoppingList = JSON.parse(localStorage.getItem("shoppingList")) || [];
+
+function displayList() {
+  ulEl.innerHTML = "";
+  for (let i = 0; i < shoppingList.length; i++) {
+    ulEl.innerHTML += "<li>" + shoppingList[i] + "</li>";
+  }
+}
+
+displayList()
+
+function handleSubmit() {
+
+    userInputColour = colour.value.trim()
+
+    if(userInputColour === "") {
+        return
+    } 
+
+    shoppingList.push(userInputColour)
+    localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
+
+    ulEl.innerHTML = ""
+    console.log(shoppingList)
+    console.log(shoppingList.length)
+    for (let i = 0; i < shoppingList.length; i++) {
+        ulEl.innerHTML += "<li>" + shoppingList[i] + "</li>";
+
+
+        // // rendering element using createElement()
+        // const li = document.createElement("li");
+        // li.textContent = shoppingList[i];
+        // ulEl.append(li);
+    }
+}
+
+addBtn.addEventListener("click", function() {
+       handleSubmit()
+})
+
+
+
+// Render the leads in the unordered list using ulEl.textContent instead of console.logging the shoppingList array items
+// for (let i = 0; i < shoppingList.length; i++) {
+
+
+
+//   ulEl.textContent += shoppingList[i] + " ";
+
+  // render the element in a li tag using innerHTML
+  // CREATING ELEMENTS WITH JAVASCRIPT (Manipulating the DOM)
+//   ulEl.innerHTML += "<li>" + shoppingList[i] + "</li>";
+
+  // rendering element using createElement()
+//   const li = document.createElement("li");
+//   li.textContent = shoppingList[i];
+//   ulEl.append(li);
+// }
+
+// //******************************************************
+
+
+
+
+
+
 
 // LocalStorage
+// Get elements
+// const inputEl = document.getElementById("input-el");
+// const inputBtn = document.getElementById("input-btn");
+// const resetBtn = document.getElementById("reset-btn");
+// const ulEl = document.getElementById("ul-el");
+
+// // Initialize the shopping list
+// let shoppingList = JSON.parse(localStorage.getItem("shoppingList")) || [];
+
+// // Function to update the display list
+// function displayList() {
+//   ulEl.innerHTML = "";
+//   for (let i = 0; i < shoppingList.length; i++) {
+//     ulEl.innerHTML += "<li>" + shoppingList[i] + "</li>";
+//   }
+// }
+
+// // Initially display the list
+// displayList();
+
+// // Add item to the list
+// inputBtn.addEventListener("click", function () {
+//   if (inputEl.value.trim() !== "") {
+//     shoppingList.push(inputEl.value.trim());
+//     localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
+//     displayList();
+//     inputEl.value = ""
+//   }
+// });
+
+// // Clear items in the list
+// resetBtn.addEventListener("dblclick", function () {
+//   localStorage.clear();
+//   shoppingList = []; // Clear the array
+//   displayList();
+// });
+
+
+
+
+
 
 
 // Thursday 
 // Hosting 
 // PDF in Portfolio Website
+// const vs let vs var
